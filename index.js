f328109d3f976270345142b148257965281a143c
neanderdev <neander.faria@hotmail.com>
2022-08-19 15:51:16 -0300

    feat: Instalação e Configuração do Emulador do Cloud Firestore

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+import "react-native-gesture-handler";
 
-import App from './App';
+import { registerRootComponent } from "expo";
+
+import "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore";
+
+if (__DEV__) {
+  firestore()
+    .terminate()
+    .then(() => {
+      firestore()
+        .clearPersistence()
+        .then(() => {
+          firestore().useEmulator("localhost", 8000);
+        })
+        .catch(() => console.error("Clear persistence error"));
+    })
+    .catch(() => console.error("Terminate error"));
+}
+
+firestore();
+
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
